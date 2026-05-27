a1afb8095a155263e19c8c4bed7285bafff2e4e0
AMROUTE <am1642271342@gmail.com>
2026-05-27 16:23:18 +0800

    1

--- Network/Model1/reinforcement_network.py
+++ Network/Model1/reinforcement_network.py
@@ -158,7 +158,7 @@ class AirSimUAVEnv(gym.Env):
         #目的地可视化开关
         self.visualize_goal = False
         # 飞行轨迹可视化开关
-        self.visualize_traj = True
+        self.visualize_traj = False
 
         # 显示用整体上移高度，单位：米
         # 注意：AirSim/Colosseum 的 z 轴是 NED 坐标，z 越小表示越高
